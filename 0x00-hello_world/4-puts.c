a7a7e6c766029c233f0063fd2c0e032a865714fc
Jupiterjil <jiloke15@gmail.com>
2022-10-13 16:39:02 -0700

    This script prints a text using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,19 @@
 #include<stdio.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 int main(void)
 {
-
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Mystr);
 
-	return 0;	
+	return (0);	
 }
