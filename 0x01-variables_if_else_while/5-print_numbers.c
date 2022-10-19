2ccbe3fc293e7c45ab0a211668b2ac51308c2f32
Jupiterjil <jiloke15@gmail.com>
2022-10-19 06:27:22 -0700

    This program prints out numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	for (n = 1; n <= 9; n++)
+	{
+		printf("%d \n", n);
+	}
+
+	return (0);
+}
