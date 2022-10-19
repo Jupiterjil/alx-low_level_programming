62170aeee47927431cf6240c5f2887a66aa759e9
Jupiterjil <jiloke15@gmail.com>
2022-10-19 04:11:16 -0700

    This project tells which random numbers are positive or negetive and equals zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,6 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
  	int n;
@@ -20,9 +19,9 @@ int main(void)
 
 	else if (n == 0)
 		printf("%d is zero\n", n);
-	
+
 	else
 		printf("%d is negative\n", n);
 
-	return (0);	
+	return (0);
 }
