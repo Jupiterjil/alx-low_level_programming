fc6a5db8922bfebb6b60ad0c757e919bb53d4f63
Jupiterjil <jiloke15@gmail.com>
2022-10-19 07:18:25 -0700

    The program prints all base 10 numbers from 9 to 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	int n = 35;
+
+	while (n < 45)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
