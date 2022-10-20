db2231207854063ff5f0ad1bac890fe856ece606
Jupiterjil <jiloke15@gmail.com>
2022-10-20 06:27:58 -0700

    This program prints the alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints out alphabets
+ *
+ * returns void
+ */
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= z; l++)
+		_putchar(l);
+
+	_putchar('\n');
+}
