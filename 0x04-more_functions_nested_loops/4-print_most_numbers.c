c29756afa10c46ee1a0991945997a260d3186a75
Jupiterjil <jiloke15@gmail.com>
2022-10-21 06:35:02 -0700

    This code prints numbers except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,8 +11,8 @@ void print_most_numbers(void)
 
 	for (n = '0'; n <= '9'; n++;)
 	{
-		if (!(c == 2 || c == 4))
-			_putchar (c);
+		if (!(n == '2' || n == '4'))
+			_putchar (n);
 	}
 	_putchar ('\n');
 }
